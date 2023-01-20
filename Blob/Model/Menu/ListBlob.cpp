1b9d4edde37e83bd51d322e9a60ec9272f9582af
Solar-311 <79645032+Solar-311@users.noreply.github.com>
2023-01-20 07:44:54 +0100

    lk

--- Blob/Model/Menu/ListBlob.cpp
+++ Blob/Model/Menu/ListBlob.cpp
@@ -9,7 +9,7 @@ ListBlob::ListBlob()
     this->listeBlob.emplace_back(new Blob("Freddy",69,new Plante()));
     this->listeBlob.emplace_back(new Blob("Thwomp",130,new Plante()));
     this->listeBlob.emplace_back(new Blob("Franck",85,new Feu()));
-    //this->listeBlob.emplace_back(new Blob("Maskass",));
+    this->listeBlob.emplace_back(new Blob("Maskass",));
     this->listeBlob.emplace_back(new Blob("Carlos"));
     this->listeBlob.emplace_back(new Blob("Jordy"));
     this->listeBlob.emplace_back(new Blob("Juan"));
